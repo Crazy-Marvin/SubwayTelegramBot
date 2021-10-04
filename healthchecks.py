bf4fa616fb707faf82581cba98a7e7d716a26588
Lucifer04 <83786816+TECH-SAVVY-GUY@users.noreply.github.com>
2021-10-04 16:23:19 +0530

    Delete healthchecks.py

--- healthchecks.py
+++
@@ -1,8 +0,0 @@
-import dotenv, os, requests
-
-dotenv.load_dotenv()
-
-try:
-    requests.get("https://hc-ping.com/db037b86-718a-4ce1-aecc-70e0a994965e", timeout=300)
-except requests.RequestException as e:
-    print("Ping failed: %s" % e)
